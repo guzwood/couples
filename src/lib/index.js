58c03e3e4743cee8abad264c9a6ae526a92168a6
guzwood <sergohot71@gmail.com>
2023-03-01 02:16:30 +0300

    4.1 homework

---
+++ src/lib/index.js
@@ -0,0 +1,29 @@
+const app = document.querySelector('.app')
+
+window.application = {
+    blocks: {},
+    screens: {},
+    timers: [],
+
+
+    renderScreen: function(screenName) {
+
+        window.application.timers.forEach(id => {
+            clearInterval(id)
+        }); 
+
+        if(window.application.screens[screenName]){
+            app.innerHTML = '';
+
+            window.application.screens[screenName]();
+        }
+    },
+    renderBlock: function(blockName, container) {
+        if(window.application.blocks[blockName]){
+        window.application.blocks[blockName](container);
+        } else {
+            console.warn("Ошибка");
+        }
+    },
+    
+  }
